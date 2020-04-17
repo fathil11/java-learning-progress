e80dfd3e36b5f66479da59d874d75e7d24b29b9c
fathil11 <fathil.arham@gmail.com>
2020-04-17 18:41:51 +0700

    GreatestCommonDivisor fix

--- CodeExercises/GreatestCommonDivisor/src/GreatestCommonDivisor.java
+++ CodeExercises/GreatestCommonDivisor/src/GreatestCommonDivisor.java
@@ -1,5 +1,8 @@
 public class GreatestCommonDivisor {
     public static int getGreatestCommonDivisor(int first, int second){
+        if((first < 10) || (second < 10)){
+            return -1;
+        }
         while (first != second) {
             if(first > second) {
                 first = first - second;
