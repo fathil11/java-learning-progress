90cfae595cf64c32f024c4249d3e1bc004b7dba2
fathil11 <fathil.arham@gmail.com>
2020-04-17 17:48:54 +0700

    updating order

---
+++ CodeExercises/FirstLastDigitSum/src/FirstLastDigitSum.java
@@ -0,0 +1,16 @@
+public class FirstLastDigitSum {
+    public static int sumFirstAndLastDigit(int number){
+        if(number < 0){
+            return -1;
+        }
+
+        int lastNumber = number % 10;
+        int firstNumber = 0;
+        while(number > 0){
+            firstNumber = number % 10;
+            number /= 10;
+        }
+
+        return firstNumber + lastNumber;
+    }
+}
