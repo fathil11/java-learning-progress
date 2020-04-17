b9fc44a4b4f889c73140665b4405d3ea37f1466d
fathil11 <fathil.arham@gmail.com>
2020-04-17 22:51:32 +0700

    FlourPacker

---
+++ CodeExercises/FlourPacker/src/FlourPacker.java
@@ -0,0 +1,11 @@
+public class FlourPacker {
+    public static boolean canPack(int bigCount, int smallCount, int goal){
+        if(bigCount<0 || smallCount<0 || goal<0) return false;
+        if((bigCount*5) + smallCount<goal) return false;
+        if(bigCount==0){
+            if(smallCount>=goal) return true;
+        }
+        int remainderOfBig = goal%5;
+        return (smallCount>=remainderOfBig);
+    }
+}
