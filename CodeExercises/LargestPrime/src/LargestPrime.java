806bf78575775ae2864780d63e772476a6881869
fathil11 <fathil.arham@gmail.com>
2020-04-18 22:13:02 +0700

    GetLargetsPrimeInit

---
+++ CodeExercises/LargestPrime/src/LargestPrime.java
@@ -0,0 +1,11 @@
+public class LargestPrime {
+    public static int getLargetsPrime(int number){
+        if(number < 0) return -1;
+        int prime = -1;
+        for(int i = 1; i < number; i++){
+            if(number % prime == 0){
+
+            }
+        }
+    }
+}
