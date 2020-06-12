2ead5398902086dda4288c4149db0b8fa5829bb6
fathil11 <fathil.arham@gmail.com>
2020-06-12 10:38:24 +0700

    Cuboid Challenge

---
+++ CodeExercises/PoolArea/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Cuboid cuboid = new Cuboid(2, 3, 1);
+
+        System.out.println("Cuboid Volume -> " + cuboid.getVolume());
+    }
+}
