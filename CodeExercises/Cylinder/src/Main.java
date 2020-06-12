b7a029fd314581e1a721bad837fc38ef255fc5a0
fathil11 <fathil.arham@gmail.com>
2020-06-12 10:06:12 +0700

    Cylinder Challenge

---
+++ CodeExercises/Cylinder/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Cylinder cylinder = new Cylinder(3, 2);
+
+        System.out.println("Cylinder Volume -> " + cylinder.getVolume());
+    }
+}
